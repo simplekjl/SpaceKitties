348a38fff9ea7c6d270e1d7310085d130e742784
Jose Luis Crisostomo <jl.cs606@gmail.com>
2021-12-12 18:37:04 +0100

    updated resolver for modules

--- server/src/resolvers/resolvers.js
+++ server/src/resolvers/resolvers.js
@@ -4,12 +4,21 @@ const resolvers = {
       tracksForHome: (_, __, { dataSources }) => {
         return dataSources.trackAPI.getTracksForHome();
       },
+    track: (_, { id }, { dataSources }) => {
+        return dataSources.trackAPI.getTrack(id)
+      }
     },
     Track: {
       author: ({ authorId }, _, { dataSources }) => {
         return dataSources.trackAPI.getAuthor(authorId);
       },
+      // has reference to the parent query which in this case is the track 
+      modules: ({ id }, _, { dataSources }) => {
+        return dataSources.trackAPI.getTrackModules(id);
+      }
     },
+
+
   };
   
   module.exports = resolvers;
